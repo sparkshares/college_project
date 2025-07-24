80f9bae953044b77bbdceeb8f16b581dda2c09fc
Bishal khatri <bishalkhatri@Bishals-MacBook-Pro.local>
2025-07-24 12:26:54 +0545

    added Userfile model for the backend

--- backend/account_management/models.py
+++ backend/account_management/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+#Userfile models
 class UserFiles(models.Model):
     file_title = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files')
